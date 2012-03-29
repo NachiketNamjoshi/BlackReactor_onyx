45212bc4e6bb4535452784ef5d84fafd0b101d77
Dima Zavin <dima@android.com>
2012-03-29 20:44:06 +0100

    ARM: 7365/1: drop unused parameter from flush_cache_user_range
    commit 4542b6a0fa6b48d9ae6b41c1efeb618b7a221b2a upstream.
    
    vma isn't used and flush_cache_user_range isn't a standard macro that
    is used on several archs with the same prototype. In fact only unicore32
    has a macro with the same name (with an identical implementation and no
    in-tree users).
    
    This is a part of a patch proposed by Dima Zavin (with Message-id:
    1272439931-12795-1-git-send-email-dima@android.com) that didn't get
    accepted.
    
    Cc: Dima Zavin <dima@android.com>
    Acked-by: Catalin Marinas <catalin.marinas@arm.com>
    Signed-off-by: Uwe Kleine-König <u.kleine-koenig@pengutronix.de>
    Signed-off-by: Russell King <rmk+kernel@arm.linux.org.uk>
    Cc: Will Deacon <will.deacon@arm.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- arch/arm/kernel/traps.c
+++ arch/arm/kernel/traps.c
@@ -498,9 +498,8 @@ do_cache_op(unsigned long start, unsigned long end, int flags)
 			start = vma->vm_start;
 		if (end > vma->vm_end)
 			end = vma->vm_end;
-
 		up_read(&mm->mmap_sem);
-		flush_cache_user_range(start, end);
+		flush_cache_user_range(vma, start, end);
 		return;
 	}
 	up_read(&mm->mmap_sem);
