783a7897c9465da42c98acb8779bbb741458f51b
Andre Guedes <andre.guedes@openbossa.org>
2012-08-01 20:34:15 -0300

    Bluetooth: Fix use-after-free bug in SMP
    commit 61a0cfb008f57ecf7eb28ee762952fb42dc15d15 upstream.
    
    If SMP fails, we should always cancel security_timer delayed work.
    Otherwise, security_timer function may run after l2cap_conn object
    has been freed.
    
    This patch fixes the following warning reported by ODEBUG:
    
    WARNING: at lib/debugobjects.c:261 debug_print_object+0x7c/0x8d()
    Hardware name: Bochs
    ODEBUG: free active (active state 0) object type: timer_list hint: delayed_work_timer_fn+0x0/0x27
    Modules linked in: btusb bluetooth
    Pid: 440, comm: kworker/u:2 Not tainted 3.5.0-rc1+ #4
    Call Trace:
     [<ffffffff81174600>] ? free_obj_work+0x4a/0x7f
     [<ffffffff81023eb8>] warn_slowpath_common+0x7e/0x97
     [<ffffffff81023f65>] warn_slowpath_fmt+0x41/0x43
     [<ffffffff811746b1>] debug_print_object+0x7c/0x8d
     [<ffffffff810394f0>] ? __queue_work+0x241/0x241
     [<ffffffff81174fdd>] debug_check_no_obj_freed+0x92/0x159
     [<ffffffff810ac08e>] slab_free_hook+0x6f/0x77
     [<ffffffffa0019145>] ? l2cap_conn_del+0x148/0x157 [bluetooth]
     [<ffffffff810ae408>] kfree+0x59/0xac
     [<ffffffffa0019145>] l2cap_conn_del+0x148/0x157 [bluetooth]
     [<ffffffffa001b9a2>] l2cap_recv_frame+0xa77/0xfa4 [bluetooth]
     [<ffffffff810592f9>] ? trace_hardirqs_on_caller+0x112/0x1ad
     [<ffffffffa001c86c>] l2cap_recv_acldata+0xe2/0x264 [bluetooth]
     [<ffffffffa0002b2f>] hci_rx_work+0x235/0x33c [bluetooth]
     [<ffffffff81038dc3>] ? process_one_work+0x126/0x2fe
     [<ffffffff81038e22>] process_one_work+0x185/0x2fe
     [<ffffffff81038dc3>] ? process_one_work+0x126/0x2fe
     [<ffffffff81059f2e>] ? lock_acquired+0x1b5/0x1cf
     [<ffffffffa00028fa>] ? le_scan_work+0x11d/0x11d [bluetooth]
     [<ffffffff81036fb6>] ? spin_lock_irq+0x9/0xb
     [<ffffffff81039209>] worker_thread+0xcf/0x175
     [<ffffffff8103913a>] ? rescuer_thread+0x175/0x175
     [<ffffffff8103cfe0>] kthread+0x95/0x9d
     [<ffffffff812c5054>] kernel_threadi_helper+0x4/0x10
     [<ffffffff812c36b0>] ? retint_restore_args+0x13/0x13
     [<ffffffff8103cf4b>] ? flush_kthread_worker+0xdb/0xdb
     [<ffffffff812c5050>] ? gs_change+0x13/0x13
    
    This bug can be reproduced using hctool lecc or l2test tools and
    bluetoothd not running.
    
    Signed-off-by: Andre Guedes <andre.guedes@openbossa.org>
    Signed-off-by: Gustavo Padovan <gustavo.padovan@collabora.co.uk>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- net/bluetooth/smp.c
+++ net/bluetooth/smp.c
@@ -293,6 +293,38 @@ static const u8	gen_method[5][5] = {
 	{CFM_PASSKEY, CFM_PASSKEY, REQ_PASSKEY, JUST_WORKS, CFM_PASSKEY},
 	{JUST_WORKS,  JUST_CFM,    JUST_WORKS,  JUST_WORKS, JUST_CFM},
 	{CFM_PASSKEY, CFM_PASSKEY, REQ_PASSKEY, JUST_WORKS, OVERLAP}
+
+static void smp_failure(struct l2cap_conn *conn, u8 reason, u8 send)
+{
+	struct hci_conn *hcon = conn->hcon;
+
+	if (send)
+		smp_send_cmd(conn, SMP_CMD_PAIRING_FAIL, sizeof(reason),
+								&reason);
+
+	clear_bit(HCI_CONN_ENCRYPT_PEND, &conn->hcon->flags);
+	mgmt_auth_failed(conn->hcon->hdev, conn->dst, hcon->type,
+			 hcon->dst_type, reason);
+
+	cancel_delayed_work_sync(&conn->security_timer);
+
+	if (test_and_clear_bit(HCI_CONN_LE_SMP_PEND, &conn->hcon->flags))
+		smp_chan_destroy(conn);
+}
+
+#define JUST_WORKS	0x00
+#define JUST_CFM	0x01
+#define REQ_PASSKEY	0x02
+#define CFM_PASSKEY	0x03
+#define REQ_OOB		0x04
+#define OVERLAP		0xFF
+
+static const u8 gen_method[5][5] = {
+	{ JUST_WORKS,  JUST_CFM,    REQ_PASSKEY, JUST_WORKS, REQ_PASSKEY },
+	{ JUST_WORKS,  JUST_CFM,    REQ_PASSKEY, JUST_WORKS, REQ_PASSKEY },
+	{ CFM_PASSKEY, CFM_PASSKEY, REQ_PASSKEY, JUST_WORKS, CFM_PASSKEY },
+	{ JUST_WORKS,  JUST_CFM,    JUST_WORKS,  JUST_WORKS, JUST_CFM    },
+	{ CFM_PASSKEY, CFM_PASSKEY, REQ_PASSKEY, JUST_WORKS, OVERLAP     },
 };
 
 static int tk_request(struct l2cap_conn *conn, u8 remote_oob, u8 auth,
