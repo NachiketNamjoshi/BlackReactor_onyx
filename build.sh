7d6d67e787e039741010a96470bfc01bc8128ed5
NachiketNamjoshi <nachiketnamjoshi@gmail.com>
2016-08-15 23:05:08 +0530

    BlackReactor: Update Version

--- build.sh
+++ build.sh
@@ -29,7 +29,7 @@ nocol='\033[0m'
 KERNEL_DIR=$PWD
 KERN_IMG=$KERNEL_DIR/arch/arm/boot/zImage
 OUT_DIR=$KERNEL_DIR/zipping/onyx
-REACTOR_VERSION="alpha-4"
+REACTOR_VERSION="alpha-6"
 
 # Device Spceifics
 export ARCH=arm
