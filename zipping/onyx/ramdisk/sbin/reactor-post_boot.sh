c32106ecb5fdfef13dac6f80e5a56aaaf29c99a8
NachiketNamjoshi <nachiketnamjoshi@gmail.com>
2016-08-21 17:43:49 +0530

    post_boot: Log for blackReactor_sound

--- zipping/onyx/ramdisk/sbin/reactor-post_boot.sh
+++ zipping/onyx/ramdisk/sbin/reactor-post_boot.sh
@@ -11,6 +11,7 @@ REACTOR_LOGFILE="$REACTOR_DATA/reactor.log"
 REACTOR_STARTCONFIG="/data/.black_reactor/startconfig"
 REACTOR_STARTCONFIG_EARLY="/data/.black_reactor/startconfig_early"
 REACTOR_STARTCONFIG_DONE="/data/.black_reactor/startconfig_done"
+REACTOR_LOGFILE_SOUND="$REACTOR_DATA/reactor_sound.log"
 
 # block devices
 SYSTEM_DEVICE="/dev/block/platform/msm_sdcc.1/by-name/system"
@@ -43,6 +44,26 @@ fi
 	echo "=========================" >> $REACTOR_LOGFILE
 	/sbin/busybox grep ro.build.version /system/build.prop >> $REACTOR_LOGFILE
 echo "=========================" >> $REACTOR_LOGFILE
+
+
+# maintain sound log file history
+	rm $REACTOR_LOGFILE_SOUND.3
+	mv $REACTOR_LOGFILE_SOUND.2 $REACTOR_LOGFILE_SOUND.3
+	mv $REACTOR_LOGFILE_SOUND.1 $REACTOR_LOGFILE_SOUND.2
+	mv $REACTOR_LOGFILE_SOUND $REACTOR_LOGFILE_SOUND.1
+
+# Initialise the sound log file (chmod to make it readable via /sdcard link)
+echo $(date) BlackReactor-Kernel initialization started > $REACTOR_LOGFILE_SOUND
+/sbin/busybox chmod 666 $REACTOR_LOGFILE_SOUND
+echo -e "\n============================================\n" >> REACTOR_LOGFILE_SOUND
+echo -e "\n**** blackReactor_sound\n" >> REACTOR_LOGFILE_SOUND
+echo "\n============================================\n" >> REACTOR_LOGFILE_SOUND
+cd /sys/class/misc/blackReactor_sound
+/sbin/busybox find * -print -maxdepth 0 -type f -exec tail -v -n +1 {} + >> REACTOR_LOGFILE_SOUND
+echo "\n============================================\n" >> REACTOR_LOGFILE_SOUND
+
+
+
 # set busybox selinux labels
 mount -o rw,remount rootfs /
 	chcon u:object_r:toolbox_exec:s0 /sbin/busybox
