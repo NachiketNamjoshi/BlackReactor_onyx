5fec433932b610d45d59e7ec6f8d11e85469aa9e
NachiketNamjoshi <nachiketnamjoshi@gmail.com>
2016-09-13 20:12:40 +0530

    BlackReactor: alpha-8.0.5

--- build.sh
+++ build.sh
@@ -29,7 +29,7 @@ nocol='\033[0m'
 KERNEL_DIR=$PWD
 KERN_IMG=$KERNEL_DIR/arch/arm/boot/zImage
 OUT_DIR=$KERNEL_DIR/zipping/onyx
-REACTOR_VERSION="alpha-8.0.4"
+REACTOR_VERSION="alpha-8.0.5"
 KERN_DTB=$KERNEL_DIR/arch/arm/boot/dt.img
 
 # Device Spceifics
