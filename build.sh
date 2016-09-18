d95c2c021cf31317b27e6ee8f75c61db59944f04
NachiketNamjoshi <nachiketnamjoshi@gmail.com>
2016-09-18 18:10:15 +0530

    BlackReactor: stable-0.1
    Signed-off-by: NachiketNamjoshi <nachiketnamjoshi@gmail.com>

--- build.sh
+++ build.sh
@@ -29,7 +29,7 @@ nocol='\033[0m'
 KERNEL_DIR=$PWD
 KERN_IMG=$KERNEL_DIR/arch/arm/boot/zImage
 OUT_DIR=$KERNEL_DIR/zipping/onyx
-REACTOR_VERSION="beta-1.7"
+REACTOR_VERSION="stable-0.1"
 KERN_DTB=$KERNEL_DIR/arch/arm/boot/dt.img
 
 # Device Spceifics
