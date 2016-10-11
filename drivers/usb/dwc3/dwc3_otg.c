2f273a97150574e974bb41065ba8e8148fc2fd0e
NachiketNamjoshi <nachiketnamjoshi@gmail.com>
2016-10-11 18:02:08 +0530

    usb: Implement USB Fast Charging Driver

--- drivers/usb/dwc3/dwc3_otg.c
+++ drivers/usb/dwc3/dwc3_otg.c
@@ -21,6 +21,10 @@
 #include <linux/boot_mode.h>
 #include <linux/kthread.h>
 
+#ifdef CONFIG_FORCE_FAST_CHARGE
+#include <linux/fastchg.h>
+#endif
+
 #include "core.h"
 #include "dwc3_otg.h"
 #include "io.h"
@@ -573,7 +577,14 @@ static int dwc3_otg_set_power(struct usb_phy *phy, unsigned mA)
 
 	if (dotg->charger->max_power == mA)
 		return 0;
-
+ #ifdef CONFIG_FORCE_FAST_CHARGE
+ 	if (force_fast_charge > 0 && mA > 0) {
+ 		mA = FAST_CHG_MAX;
+ 		pr_info("USB fast charging is ON\n");
+ 	} else {
+ 		pr_info("USB fast charging is OFF\n");
+ 	}
+#endif
 	dev_info(phy->dev, "Avail curr from USB = %u\n", mA);
 #ifdef VENDOR_EDIT
       if(get_boot_mode()==MSM_BOOT_MODE__RF){
