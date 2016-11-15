fb50904426a29d1897442d73e090ee579725556f
NachiketNamjoshi <nachiketnamjoshi@gmail.com>
2016-11-15 20:52:07 +0530

    BlackReactor: stable-0.4

--- build.sh
+++ build.sh
@@ -29,7 +29,7 @@ nocol='\033[0m'
 KERNEL_DIR=$PWD
 KERN_IMG=$KERNEL_DIR/arch/arm/boot/zImage-dtb
 OUT_DIR=$KERNEL_DIR/zipping/onyx
-REACTOR_VERSION="beta-12.2"
+REACTOR_VERSION="stable-0.4"
 
 # Device Spceifics
 export ARCH=arm
