92db0b6d7bbdcaa73a2a20cde07016c056c18e53
NachiketNamjoshi <nachiketnamjoshi@gmail.com>
2018-01-09 13:08:43 +0530

    adblocker: farewell, friend.
    Signed-off-by: NachiketNamjoshi <nachiketnamjoshi@gmail.com>

--- build.sh
+++ build.sh
@@ -65,7 +65,6 @@ then
 echo -e "$red Kernel Compilation failed! Fix the errors! $nocol"
 exit 1
 fi
-block_ads
 zipping
 get_md5
 }
@@ -76,27 +75,6 @@ zipping() {
 cp $KERN_IMG $OUT_DIR/zImage
 cd $OUT_DIR
 zip -r -9 BlackReactor-onyx-$REACTOR_VERSION-$(date +"%Y%m%d")-$(date +"%H%M%S").zip *
-}
-block_ads() {
-HOSTS_FILE="$OUT_DIR/system/hosts"
-HOST_FILE="$OUT_DIR/system/host"
-rm -rf "$HOSTS_FILE"
-wget -O $HOST_FILE"4" "http://pgl.yoyo.org/adservers/serverlist.php?hostformat=hosts&showintro=0&mimetype=plaintext"
-wget -O $HOST_FILE"3" "http://adaway.org/hosts.txt"
-cat $HOST_FILE"4" >> $HOST_FILE"3"; rm -rf $HOST_FILE"4"
-wget -O $HOST_FILE"2" "http://hosts-file.net/ad_servers.txt"
-cat $HOST_FILE"3" >> $HOST_FILE"2"; rm -rf $HOST_FILE"3"
-wget -O $HOST_FILE"1" "http://winhelp2002.mvps.org/hosts.txt"
-cat $HOST_FILE"2" >> $HOST_FILE"1"; rm -rf $HOST_FILE"2"
-sed '/^#/ d' $HOST_FILE"1" > $HOST_FILE; 
-rm -rf $HOST_FILE"1"
-sort $HOST_FILE | uniq -u > $HOSTS_FILE; rm -rf $HOST_FILE
-sed '/localhost/d' $HOSTS_FILE > $HOST_FILE; rm -rf $HOSTS_FILE
-sed -i -e 's/0.0.0.0/127.0.0.1/g' $HOST_FILE; sed -i '1i #adblocker' $HOST_FILE
-sed -i '2i 127.0.0.1 localhost' $HOST_FILE; sed -i '3i ::1 localhost' $HOST_FILE
-awk '{$1=$1}1' OFS=" " $HOST_FILE > $HOSTS_FILE
-sed -i -e '$a\' $HOSTS_FILE
-rm -rf $HOST_FILE
 }
 
 get_md5() {
